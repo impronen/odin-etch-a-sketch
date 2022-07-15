9b2c03ab7a379c0431a358be588afae05ef39540
impronen <impronen@gmail.com>
2022-07-15 10:34:38 +0300

    GRID funtion attached to div main

--- scripts.js
+++ scripts.js
@@ -1,14 +1,29 @@
-/* 
+/*
+const container = document.querySelector('#main');
 
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
 
-
-
+container.appendChild(content);
 */
-document.body.onload = genGrid(16,16);
 
+// 
+// give a variable for the main container
+
+
+const container = document.querySelectorAll('main');
+
+const gridContainer = document.createElement('div');
+gridContainer.classList.add('gridContainer');
+gridContainer.addEventListener
+
+main.appendChild(gridContainer);
+
+container.addEventListener("onload", genGrid(16,16))
 
 function genGrid(v, b){
-let e = document.body;
+let e = gridContainer;
 
     for(let i = 0; i < v; i++) {
         let row = document.createElement("row-divs");

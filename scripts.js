ebb104286a37db261238bf4cbd5f20eed46da841
impronen <impronen@gmail.com>
2022-07-19 11:33:13 +0300

    Revert from incorrect line of investigation

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 
-//Select div main and set it as constant container
+//Select div main and set it as constant "container"
 const container = document.querySelectorAll('main'); 
 
 //Create a new div and give it a class
@@ -10,19 +10,19 @@ main.appendChild(gridContainer);
 //Run function on loading page and place resulting rows/column inside gridContainer
 container.addEventListener("onload", genGrid(16,16))
 
-document.querySelector('.gridCell').addEventListener('onclick', changeColor);
-document.querySelector('.gridCell').addEventListener('mouseleave', changeColor );
+const myList = gridContainer.childNodes;
+
 
 //Function to generate a grid
 function genGrid(v, b){
 let e = gridContainer;
 
     for(let i = 0; i < v; i++) { //Creates row-divs until number of divs = 1st parameter
-        let row = document.createElement("row-divs");
+        let row = document.createElement("div");
         row.className = "row";
 
         for(let x = 1; x <= b; x++){ // Creates cell-divs and makes them child of row-divs
-            let cell = document.createElement("cell-divs"); // until 2nd parameter is reached
+            let cell = document.createElement("div"); // until 2nd parameter is reached
             cell.className = "gridCell";
             // cell.innerText = "D";
             row.appendChild(cell);
@@ -33,8 +33,6 @@ let e = gridContainer;
 
 //Function to change cell-div's class
 
-function changeColor() {
-    this.classList.toggle('red');
-    this.classList.toggle('green');
-}
-
+You can't add an event listener to an array of node elements.
+You have to loop through them and do it instead.
+https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach

e2147b5d4e3f1ffd34b981021d608ccbe815aa2b
impronen <impronen@gmail.com>
2022-07-30 13:30:24 +0300

    RETURN to previous function for grid generation
    WILL now continue with problem of how to manipulate and style

--- scripts.js
+++ scripts.js
@@ -1,18 +1,26 @@
 
+const gridContainer = document.querySelector('.gridContainer');
+let gridValue = document.querySelector('.grid-size');
+let gridMeasures = document.querySelector('input');
+const resetBtn = document.querySelector('.Reset');
+const generate = document.querySelector('.generate');
+let squareSize = 8;
 
-const container = document.querySelectorAll('main'); 
+
+const container = document.querySelectorAll('gridContainer'); 
+
+const grid = document.createElement('div');
+grid.classList.add('grid');
+gridContainer.appendChild(grid);
+
+const gridManipulation = document.querySelectorAll('gridContainer');
 
 
-const gridContainer = document.createElement('div');
-gridContainer.classList.add('gridContainer');
-main.appendChild(gridContainer);
-
-container.addEventListener("onload", genGrid(16,16))
+container.addEventListener("onload", genGrid(8,8));
 
 
 //Function to generate a grid
 function genGrid(v, b){
-let e = gridContainer;
 
     for(let i = 0; i < v; i++) { 
         let row = document.createElement("div");
@@ -23,30 +31,58 @@ let e = gridContainer;
             cell.className = "gridCell";
             row.appendChild(cell);
         }
-        e.appendChild(row);
+        gridContainer.appendChild(row);
     }
 }
 
-//Function to change cell-div's class
+/* NOT MY CODE - for reference from https://github.com/TarekVisch/etch-a-sketch
+
+// To create square div based on size value handed over by either default or slider
+
+function divGeneration(size) {
+  const div = document.createElement('div');
+  div.classList.add('gridContainer');
+  div.style.width = `${size}px`;
+  div.style.height = `${size}px`;
+
+  return div;
+}
+
+// Creat The Grid and append it to grid - place previous function inside this one
+
+
+
+function gridGeneration(gridMeasures) {
+  for (let i = 0; i < gridMeasures; i++) {
+    for (let x = 0; x < gridMeasures; x++) {
+      gridContainer.appendchild(divGeneration(gridContainer.clientwidth / gridMeasures));
+    }
+  }
+}
+*/
+
+//Reset - removes children of the grid div and runs the grid creation again
+
+
+
+
+
+
+
+
+
+
+
+/*
+gridManipulation.forEach((gridContainer) => {
+
+        gridManipulation.addEventListener('click', () => {
+            alert(gridManipulation.className);
+        });
+});
 
 /*
 You can't add an event listener to an array of node elements.
 You have to loop through them and do it instead.
 https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach
 */
-
-
-const gridManipulation = document.querySelectorAll('.gridCell') // Creates a variable with the nodelist
-
-//Then use forEach on this bastard to do something to each node on the list
-
-// .addEventListener('onclick', testFunction);
-
-
-function changeColor() {
-    this.classList.toggle('red');
-}
-
-function testFunction() {
-    alert("Hello! I am an alert box!!");
-}
